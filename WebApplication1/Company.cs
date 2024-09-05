69213e93f0597d4db8849406c93c2c45b01c78dd
powerofm1nd <powerofmind@tuta.io>
2024-09-05 17:57:08 +0300

    up

--- WebApplication1/Company.cs
+++ WebApplication1/Company.cs
@@ -2,12 +2,12 @@ namespace WebApplication1;
 
 public class Company
 {
-    string Name {get; set;}
-    string Address {get; set;}
-    string City {get; set;}
-    string State {get; set;}
-    string ZipCode {get; set;}
-    string Phone {get; set;}
+    string Name { get; set;}
+    string Address { get; set;}
+    string City { get; set;}
+    string State { get; set;}
+    string ZipCode { get; set;}
+    string Phone { get; set;}
 
     string ToString()
     {
